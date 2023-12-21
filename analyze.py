65a8436f4b5a115ddeb1aeb2ae303a7a1bf1eb38
Khemarato Bhikkhu <theopenbuddhistuniversity@gmail.com>
2023-12-21 09:08:59 +0700

    dont double count final recall rate

--- analyze.py
+++ analyze.py
@@ -8,6 +8,7 @@ from main import (
 
 entries = []
 tags = []
+tag_likes = defaultdict(list)
 count = 0.0
 liked = 0.0
 with yaspin(text='Crunching the numbers...'):
@@ -15,29 +16,28 @@ with yaspin(text='Crunching the numbers...'):
         if fd.name.endswith('.json'):
             entries.append(FeedEntry(json_file=fd))
     tag_count = defaultdict(float)
-    tag_likes = defaultdict(float)
     for entry in entries:
         count += 1.0
         inc = 0.0
         if entry.status == 'liked':
             liked += 1.0
-            inc = 1.0
         for tag in entry.tags:
             tag_count[tag] += 1.0
-            tag_likes[tag] += inc
+            if entry.status == 'liked':
+                tag_likes[tag].append(entry)
     tags = list(tag_count.keys())
-    tags.sort(key=lambda k: tag_likes[k]/tag_count[k], reverse=True)
-    tags = [k for k in tags if tag_likes[k]>1 and tag_count[k]>1 and tag_likes[k]/tag_count[k]>liked/count]
+    tags.sort(key=lambda k: len(tag_likes[k])/tag_count[k], reverse=True)
+    tags = [k for k in tags if len(tag_likes[k])>1 and tag_count[k]>1 and len(tag_likes[k])/tag_count[k]>liked/count]
 r = min(30, len(tags))
 print(f"So far you've liked {int(liked)}/{int(count)}={liked*100.0/count:.1f}% of the posts.")
 if r > 0:
   print(f"""Top {r} tags are:""")
-  toplikes = 0
+  toplikes = set()
   for i in range(r):
     tag = tags[i]
-    toplikes += tag_likes[tag]
-    print(f"{i+1}. {tag} ({int(tag_likes[tag])}/{int(tag_count[tag])}={100.0*tag_likes[tag]/tag_count[tag]:.1f}%)")
-  print(f"\nSubscribing to just these would have given you {int(toplikes)}/{int(liked)}={toplikes*100.0/liked:.1f}% of the posts you've liked.")
+    toplikes.update(tag_likes[tag])
+    print(f"{i+1}. {tag} ({len(tag_likes[tag])}/{int(tag_count[tag])}={100.0*len(tag_likes[tag])/tag_count[tag]:.1f}%)")
+  print(f"\nSubscribing to just these would have given you {len(toplikes)}/{int(liked)}={len(toplikes)*100.0/liked:.1f}% of the posts you've liked.")
 else:
   print("Insufficient data to recommend even a single tag.")
 
