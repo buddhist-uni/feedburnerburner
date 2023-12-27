19583bfb45486d773a862b3a7f2269a3b8266229
Khemarato Bhikkhu <theopenbuddhistuniversity@gmail.com>
2023-12-27 14:21:23 +0700

    fix bug

--- analyze.py
+++ analyze.py
@@ -37,7 +37,7 @@ with yaspin(text='Crunching the numbers...'):
     tags.sort(key=lambda k: len(tag_likes[k])/tag_count[k], reverse=True)
     domains.sort(key=lambda k: len(domain_likes[k])/domain_counts[k], reverse=True)
     tags = [k for k in tags if len(tag_likes[k])>1 and tag_count[k]>1 and len(tag_likes[k])/tag_count[k]>liked/count]
-    domains = [k for k in domains if len(domain_likes[domain])>1 and len(domain_likes[k])/domain_counts[k]>liked/count]
+    domains = [k for k in domains if len(domain_likes[k])>1 and len(domain_likes[k])/domain_counts[k]>liked/count]
 r = min(30, len(tags))
 print(f"So far you've liked {int(liked)}/{int(count)}={liked*100.0/count:.1f}% of the posts.")
 if r > 0:
@@ -47,13 +47,14 @@ if r > 0:
     tag = tags[i]
     toplikes.update(tag_likes[tag])
     print(f"{i+1}. {tag} ({len(tag_likes[tag])}/{int(tag_count[tag])}={100.0*len(tag_likes[tag])/tag_count[tag]:.1f}%)")
-  print(f"\nSubscribing to just these would have given you {len(toplikes)}/{int(liked)}={len(toplikes)*100.0/liked:.1f}% of the posts you've liked.")
   r = min(30, len(domains))
   if r > 0:
      print(f"\nYour {r} top-liked domains are:")
      for i in range(r):
         domain = domains[i]
+        toplikes.update(domain_likes[domain])
         print(f"{i+1}. {domain} ({len(domain_likes[domain])}/{int(domain_counts[domain])}={len(domain_likes[domain])*100.0/domain_counts[domain]:.1f}%)")
+  print(f"\nSubscribing to just these would have given you {len(toplikes)}/{int(liked)}={len(toplikes)*100.0/liked:.1f}% of the posts you've liked.")
 else:
   print("Insufficient data to recommend even a single tag.")
 
